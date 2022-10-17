53374f38392e8dfe05379843bd3e1e2a2a493955
Anthony S <anthcomercial@Gmail.com>
2022-10-17 11:53:55 -0300

    development/homepage-working

--- main_code/webpage/urls.py
+++ main_code/webpage/urls.py
@@ -1,6 +1,9 @@
+from unicodedata import name
 from django.urls import path
-from .views import mainPage
+from .views import mainPage, homePage, aboutPage
 
 urlpatterns = [
     path('', mainPage, name='webpage-view'),
+    path('entenda-melhor/', homePage, name='homepage-view'),
+    path('sobre/', aboutPage, name='aboutpage-view'),
 ]
--- main_code/webpage/views.py
+++ main_code/webpage/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def mainPage(request):
+    return render(request, 'index.html')
+
+def homePage(request):
     return render(request, 'mainpage.html')
+
+def aboutPage(request):
+    return render(request, 'about.html')
