e744b26973d0821a3b5cc7d171075a5d0e50e429
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2022-10-18 18:00:49 -0300

    att

--- main_code/webpage/models.py
+++ main_code/webpage/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Endereco(models.Model):
+    rua = models.CharField()
+    bairro = models.CharField()
+    numero = models.IntegerField()
+    complemento = models.CharField()
+
+
+class Funcionario(models.Model):
+    primeiro_nome = models.CharField(max_length=128)
+    sobrenome = models.CharField(max_length=128)
+    cpf = models.IntegerField(max_length=11)
+    idade = models.IntegerField(max_length=3)
+    nascimento = models.DateTimeField(auto_now_add=True)
+    endereco = models.ForeignKey(Endereco, on_delete=models.DO_NOTHING, related_name="funcionario")
+
+
+"""
+Primeiro nome
+    Sobrenome
+    Data de admissão
+    Idade
+    Sexo
+    Data de aniversário
+    Endereço
+    CPF
+"""
--- main_code/webpage/urls.py
+++ main_code/webpage/urls.py
@@ -1,12 +1,14 @@
 from unicodedata import name
 from django.urls import path
 from .views import mainPage, homePage, aboutPage, colaboradores
+from . import views
 
-app_name = 'webpage'
+app
 
 urlpatterns = [
     path('', mainPage, name='webpage-view'),
     path('entenda-melhor/', homePage, name='homepage-view'),
     path('sobre/', aboutPage, name='aboutpage-view'),
     path('colaboradores/',colaboradores,name='colaboradores'),
+    path('',views.aboutPage)
 ]
