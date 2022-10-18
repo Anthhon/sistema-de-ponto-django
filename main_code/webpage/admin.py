4b8d2606e0b7ab3340a771cfe517bacc36877c9e
Anthony S <anthcomercial@Gmail.com>
2022-10-18 05:14:50 -0500

    development/employee-models-working

--- main_code/webpage/admin.py
+++ main_code/webpage/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Funcionario
 
 # Register your models here.
+admin.site.register(Funcionario)
--- main_code/webpage/models.py
+++ main_code/webpage/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+from django.db.models import CharField, DateField, IntegerField, Model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Funcionario(Model):
+    nome = CharField(max_length=12)
+    sobrenome = CharField(max_length=15)
+    admissão = DateField()
+    cargo = CharField(max_length=25)
+    idade = IntegerField(
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(10)
+        ]
+     )
+    gênero = CharField(max_length=20)
+    aniversário = DateField()
+    endereço = CharField(max_length=75)
+    CPF = IntegerField(max_length=11)
+
+# Coloca o nome da tarefa como o próprio título na tela de administrador
+    def __str__(self):
+        return (f"{self.nome} {self.sobrenome} - {self.cargo}")
+
+'''
+    Primeiro nome
+    Sobrenome
+    Data de admissão
+    Idade
+    Sexo
+    Data de aniversário
+    Endereço
+    CPF
+'''
