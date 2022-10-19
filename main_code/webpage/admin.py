24b78cf75f3f8575e71d3c429f9a37ec8fe77c57
Anthony S <anthcomercial@Gmail.com>
2022-10-19 10:30:17 -0300

    development/models-update-working

--- main_code/webpage/admin.py
+++ main_code/webpage/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Funcionario
+from .models import Cargo, Funcionario, Gênero
+
 
 # Register your models here.
 admin.site.register(Funcionario)
+admin.site.register(Cargo)
+admin.site.register(Gênero)
--- main_code/webpage/models.py
+++ main_code/webpage/models.py
@@ -1,20 +1,29 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.db.models import CharField, DateField, IntegerField, Model
+from django.db.models import CharField, DateField, IntegerField, Model, ForeignKey, RESTRICT, CASCADE
 
+class Gênero(Model):
+    gênero = CharField(max_length=35)
+    def __str__(self):
+        return self.gênero
+
+class Cargo(Model):
+    cargo = CharField(max_length=50)
+    def __str__(self):
+        return self.cargo
 
 # Create your models here.
 class Funcionario(Model):
     nome = CharField(max_length=12)
     sobrenome = CharField(max_length=15)
     admissão = DateField()
-    cargo = CharField(max_length=25)
+    cargo = ForeignKey(Cargo, on_delete=RESTRICT)
     idade = IntegerField(
         validators=[
             MaxValueValidator(100),
             MinValueValidator(10)
         ]
      )
-    gênero = CharField(max_length=20)
+    gênero = ForeignKey(Gênero, on_delete=RESTRICT)
     aniversário = DateField()
     endereço = CharField(max_length=75)
     CPF = IntegerField()#max_length' is ignored when used with IntegerField.
