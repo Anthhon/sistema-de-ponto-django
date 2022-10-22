ba74cdb5f1b51b115627cf6a337315f5b7d66f5d
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2022-10-22 08:56:19 -0300

    Pagina principal configurada

--- main_code/webpage/models.py
+++ main_code/webpage/models.py
@@ -1,9 +1,9 @@
-from email.policy import default
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db.models import (CharField, DateField, IntegerField, Model, ForeignKey, TimeField, RESTRICT, CASCADE)
-from datetime import datetime
+from datetime import datetime, time
 from django.db import models
 from django import forms
+
 
 NOW = datetime.now
 
@@ -44,10 +44,11 @@ class CheckIn(Model):
     #apesar do python aceitar pontuações não coloque, pois as urls tem problemas com pontuações
     funcionario = ForeignKey(Funcionario, on_delete=RESTRICT)
     dia = DateField(default=NOW)
-    entrada_manha = TimeField(auto_now=False, auto_now_add=False, default=None, null=True,blank=True)
-    saida_manha = TimeField(auto_now=False, auto_now_add=False, default=None, null=True,blank=True)
-    entrada_tarde = TimeField(auto_now=False, auto_now_add=False, default=None, null=True,blank=True)
-    saida_tarde = TimeField(auto_now=False, auto_now_add=False, default=None, null=True,blank=True)
+    """  """
+    entrada_manha = TimeField(auto_now=False, auto_now_add=False, default=time(0,0), null=True,blank=True)
+    saida_manha = TimeField(auto_now=False, auto_now_add=False, default=time(0,0), null=True,blank=True)
+    entrada_tarde = TimeField(auto_now=False, auto_now_add=False, default=time(0,0), null=True,blank=True)
+    saida_tarde = TimeField(auto_now=False, auto_now_add=False, default=time(0,0), null=True,blank=True)
 
     def __str__(self):
         return (f"{self.funcionario} - {self.dia}")
