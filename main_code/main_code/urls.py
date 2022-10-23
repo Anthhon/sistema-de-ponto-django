ca0acb564aba5629278d93e3e349aa067cf18a7f
Anthony S <anthcomercial@Gmail.com>
2022-10-23 05:07:33 -0500

    ui/other-changes

--- main_code/main_code/urls.py
+++ main_code/main_code/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+app_name = "mainurls"
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name="admin-page"),
     path('', include('webpage.urls')),
 ]
